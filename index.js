8a73e125e0bd45e3a65217faf5f98d9e127d4123
abdulsamee56 <sameeabdul56@gmail.com>
2023-05-20 11:44:11 -0400

    Adding operation functions and event handling

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let buttons = document.querySelector('.buttons');
+let inp = document.querySelector('.input-s');
 
 const buttonLabels = [
     '7', '8', '9', '/',
@@ -11,5 +12,30 @@ const buttonLabels = [
 buttonLabels.forEach(l =>{
     let creator = document.createElement('button');
     creator.textContent=l;
+
+    inp.textContent='';
+    creator.addEventListener('click',()=>{
+        if(creator.textContent != '=' )
+        inp.textContent += creator.textContent;
+    })
+
     buttons.appendChild(creator);
 })
+
+
+function add(first,  second){
+    return parseInt(first) + parseInt(second) ;
+}
+
+function subtract(first,  second){
+    return parseInt(first) - parseInt(second) ;
+}
+
+
+function multiply(first,  second){
+    return parseInt(first) * parseInt(second) ;
+}
+
+function division(first,  second){
+    return parseInt(first) / parseInt(second) ;
+}
